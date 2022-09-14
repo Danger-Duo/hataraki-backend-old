4b76c9f05ae35417693733b76f254d29318b1ae2
don-tay <dontay0209@gmail.com>
2022-09-14 17:29:25 +0800

    Fix routing for job listing and application

--- src/main/java/com/hataraki/backend/jobapplication/JobApplicationController.java
+++ src/main/java/com/hataraki/backend/jobapplication/JobApplicationController.java
@@ -2,14 +2,18 @@ package com.hataraki.backend.jobapplication;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/job-application")
 public class JobApplicationController {
-    @PostMapping("/job-application")
-    public JobApplication jobApplication(@RequestBody JobApplication jobApplication) {
-        return new JobApplication(jobApplication.getId(), jobApplication.getFirstName(),
-                jobApplication.getLastName(), jobApplication.getResumeLink(), jobApplication.getCreatedBy(),
+    @PostMapping
+    public JobApplication createJobApplication(@RequestBody JobApplication jobApplication) {
+        return new JobApplication(jobApplication.getId(),
+                jobApplication.getFirstName(),
+                jobApplication.getLastName(), jobApplication.getResumeLink(),
+                jobApplication.getCreatedBy(),
                 jobApplication.getDateApplied());
     }
 }
--- src/main/java/com/hataraki/backend/joblisting/JobListingController.java
+++ src/main/java/com/hataraki/backend/joblisting/JobListingController.java
@@ -2,14 +2,15 @@ package com.hataraki.backend.joblisting;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/job-listing")
 public class JobListingController {
-
-    // create job listing endpoint
-    @PostMapping("/job-listing")
+    @PostMapping
     public JobListing createJobListing(@RequestBody JobListing jobListing) {
-        return new JobListing(jobListing.getId(), jobListing.getTitle(), jobListing.getDescription(), jobListing.getCreatedBy(), jobListing.getDatePosted());
+        return new JobListing(jobListing.getId(), jobListing.getTitle(), jobListing.getDescription(),
+                jobListing.getCreatedBy(), jobListing.getDatePosted());
     }
 }
