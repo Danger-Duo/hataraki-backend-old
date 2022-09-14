35cca3a8e997c0dd20aeaa737a04b73f1e8a898c
Don Tay <contact@hellodon.dev>
2022-09-14 14:12:15 +0800

    Add create job listing endpoint

--- src/main/java/com/hataraki/backend/jobapplication/JobApplicationController.java
+++ src/main/java/com/hataraki/backend/jobapplication/JobApplicationController.java
@@ -4,15 +4,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @RestController
 public class JobApplicationController {
-    private final AtomicLong counter = new AtomicLong();
-
     @PostMapping("/job-application")
     public JobApplication jobApplication(@RequestBody JobApplication jobApplication) {
-        return new JobApplication(counter.incrementAndGet(), jobApplication.getFirstName(),
+        return new JobApplication(jobApplication.getId(), jobApplication.getFirstName(),
                 jobApplication.getLastName(), jobApplication.getResumeLink(), jobApplication.getCreatedBy(),
                 jobApplication.getDateApplied());
     }
--- src/main/java/com/hataraki/backend/joblisting/JobListingController.java
+++ src/main/java/com/hataraki/backend/joblisting/JobListingController.java
@@ -1,8 +1,15 @@
 package com.hataraki.backend.joblisting;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JobListingController {
-    
+
+    // create job listing endpoint
+    @PostMapping("/job-listing")
+    public JobListing createJobListing(@RequestBody JobListing jobListing) {
+        return new JobListing(jobListing.getId(), jobListing.getTitle(), jobListing.getDescription(), jobListing.getCreatedBy(), jobListing.getDatePosted());
+    }
 }
