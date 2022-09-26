3c15c06074418dd6ae4c899da81ffd5202b6699c
Don Tay <dontay0209@gmail.com>
2022-09-26 17:50:20 +0800

    Refactor job application to service layer

--- src/main/java/com/hataraki/backend/jobapplication/JobApplicationController.java
+++ src/main/java/com/hataraki/backend/jobapplication/JobApplicationController.java
@@ -1,6 +1,5 @@
 package com.hataraki.backend.jobapplication;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,44 +12,25 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.hataraki.backend.joblisting.JobListing;
-import com.hataraki.backend.joblisting.JobListingService;
-
 @RestController
 @RequestMapping("/api/v1/job-applications")
 public class JobApplicationController {
 
     @Autowired
-    private JobApplicationRepository jobApplicationRepository;
-    @Autowired
-    private JobListingService jobListingService;
+    private JobApplicationService jobApplicationService;
 
     @GetMapping
     public List<JobApplication> getJobApplications() {
         // TODO: filter by user
-        return this.jobApplicationRepository.findAll();
+        return this.jobApplicationService.getJobApplications();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public JobApplication createJobApplication(@RequestBody CreateJobApplicationReqDto req) {
-        // TODO: get user id from token
-        String userId = "123";
         // get job listing by jobListingId, 404 response if not found
         try {
-            JobListing jl = this.jobListingService.getJobListingById(req.getJobListingId());
-            JobApplication jobApplication = new JobApplication(
-                    jl,
-                    req.getFirstName(),
-                    req.getLastName(),
-                    req.getEmail(),
-                    req.getStartDate(),
-                    req.getResumeLink(),
-                    req.getPersonalStatement(),
-                    userId,
-                    LocalDateTime.now(),
-                    LocalDateTime.now());
-            return this.jobApplicationRepository.save(jobApplication);
+            return this.jobApplicationService.createJobApplication(req);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Job Listing not found");
         }
