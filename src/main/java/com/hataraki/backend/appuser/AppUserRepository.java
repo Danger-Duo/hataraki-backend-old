7b90f98ccde7a17aa63ef731056b17346c0c9fc8
Don Tay <dontay0209@gmail.com>
2022-10-08 12:25:12 +0800

    Add appuser

--- src/main/java/com/hataraki/backend/appuser/AppUserRepository.java
+++ src/main/java/com/hataraki/backend/appuser/AppUserRepository.java
@@ -1,7 +1,11 @@
 package com.hataraki.backend.appuser;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AppUserRepository extends MongoRepository<AppUser, String> {
-  AppUser findByUsername(String username);
+  Optional<AppUser> findByUsername(String username);
+
+  Optional<AppUser> findByEmail(String email);
 }
